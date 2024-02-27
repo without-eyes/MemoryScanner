8514da1aeb5cb8092ff5cab47e2274972e9e2f3b
without eyes <37347347+without-eyes@users.noreply.github.com>
2024-02-27 11:05:28 +0200

    Finished program

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <windows.h>
 
-// I don't understand it but it is working
+// I don't fully understand this but it works
 #define IS_IN_SEARCH(mb,offset) (mb->searchmask[(offset) / 8] & 1 << ((offset) % 8))
 #define REMOVE_FROM_SEARCH(mb,offset) mb->searchmask[(offset) / 8] &= ~(1 << ((offset) % 8))
 
@@ -86,23 +86,33 @@ void update_memblock(MEMBLOCK* mb, SEARCH_CONDITION condition, unsigned int val)
                     if (IS_IN_SEARCH(mb,(total_read + offset))) {
                         BOOL is_match = FALSE;
                         unsigned int temp_val;
+                        unsigned int prev_val = 0;
 
                         switch (mb->data_size) { // In bytes
                             case 1:
                                 temp_val = tempbuf[offset];
+                                prev_val = *((unsigned char*)&mb->buffer[total_read + offset]);
                                 break;
                             case 2:
                                 temp_val = *((unsigned short*)&tempbuf[offset]);
+                                prev_val = *((unsigned short*)&mb->buffer[total_read + offset]);
                                 break;
                             case 4:
                             default:
                                 temp_val = *((unsigned int*)&tempbuf[offset]);
+                                prev_val = *((unsigned int*)&mb->buffer[total_read + offset]);
                                 break;
                         }
 
                         switch (condition) {
                             case COND_EQUALS:
                                 is_match = (temp_val == val);
+                                break;
+                            case COND_INCREASED:
+                                is_match = (temp_val > prev_val);
+                                break;
+                            case COND_DECREASED:
+                                is_match = (temp_val < prev_val);
                                 break;
                             default:
                                 break;
@@ -184,7 +194,7 @@ void dump_scan_info(MEMBLOCK* mb_list) {
     MEMBLOCK *mb = mb_list;
 
     while (mb) {
-        // %08x means: 0 - fill blank spaces with 0; 8 - output width; x - integer.
+        // %08x means: 0 - fill blank spaces with 0; 8 - output width; x - hexadecimal.
         printf("0x%08x %d\r\n", mb->addr, mb->size);
 
         for (int i = 0; i < mb->size; i++) {
@@ -196,13 +206,30 @@ void dump_scan_info(MEMBLOCK* mb_list) {
     }
 }
 
+void poke(HANDLE hProc, int data_size, size_t addr, unsigned int val) {
+    if (WriteProcessMemory(hProc, (void*)addr, &val, data_size, NULL) == 0) {
+        printf("poke failed\r\n");
+    }
+}
+
+unsigned int peek(HANDLE hProc, int data_size, size_t addr) {
+    unsigned int val = 0;
+
+    if(ReadProcessMemory(hProc, (void*)addr, &val, data_size, NULL) == 0) {
+        printf("peek failed\r\n");
+    }
+
+    return val;
+}
+
 void print_matches(MEMBLOCK* mb_list) {
     MEMBLOCK* mb = mb_list;
 
     while (mb) {
         for (unsigned int offset = 0; offset < mb->size; offset += mb->data_size) {
             if (IS_IN_SEARCH(mb,offset)) {
-                printf("08%08x\r\n", mb->addr + offset);
+                unsigned int val = peek(mb->hProc, mb->data_size, (size_t) (mb->addr + offset));
+                printf("0x%08x: 0x%08x (%d) \r\n", mb->addr + offset, val, val);
             }
         }
         mb = mb->next;
@@ -221,25 +248,121 @@ int get_match_count (MEMBLOCK *mb_list) {
     return count;
 }
 
-int main (int argc, char *argv[]) {
-    MEMBLOCK *scan = create_scan (atoi(argv[1]), 4);
+unsigned int str2int (char *s) {
+    int base = 10;
 
-    if (scan) {
-        printf("searching for 1000\r\n");
-        update_scan(scan, COND_EQUALS, 1000);
-        print_matches(scan);
-
-        {
-            char s[10];
-            gets(s);
-        }
-
-        printf("searching for 2000\r\n");
-        update_scan(scan, COND_EQUALS, 2000);
-        print_matches(scan);
-
-        free_scan(scan);
+    if (s[0] == '0' && s[1] == 'x') {
+        base = 16;
+        s += 2;
     }
 
+    return strtoul(s, NULL, base);
+}
+
+MEMBLOCK* ui_new_scan() {
+    MEMBLOCK* scan = NULL;
+    DWORD pid;
+    int data_size;
+    unsigned int start_val;
+    SEARCH_CONDITION start_cond;
+    char s[20];
+
+    while (1) {
+        printf("\r\nEnter the pid: ");
+        fgets(s, sizeof(s), stdin);
+        pid = str2int(s);
+        printf("\r\nEnter the data size: ");
+        fgets(s, sizeof(s), stdin);
+        data_size = str2int(s);
+        printf("\r\nEnter the start value, or \'u\' for unknown: ");
+        fgets(s, sizeof(s), stdin);
+        if (s[0] == 'u') {
+            start_cond = COND_UNDONDITIONAL;
+            start_val = 0;
+        } else {
+            start_cond = COND_EQUALS;
+            start_val = str2int(s);
+        }
+
+        scan = create_scan(pid, data_size);
+        if (scan) break;
+        printf("\r\nInvalid scan");
+    }
+
+    update_scan(scan, start_cond, start_val);
+    printf("\r\n%d matches found\r\n", get_match_count(scan));
+
+    return scan;
+}
+
+void ui_poke (HANDLE hProc, int data_size) {
+    unsigned int addr;
+    unsigned int val;
+    char s[20];
+
+    printf("Enter the address: ");
+    fgets(s, sizeof(s), stdin);
+    addr = str2int(s);
+
+    printf("\r\nEnter the value: ");
+    fgets(s, sizeof(s), stdin);
+    val = str2int(s);
+    printf("\r\n");
+
+    poke(hProc, data_size, addr, val);
+}
+
+void ui_run_scan () {
+    unsigned int val;
+    char s[20];
+    MEMBLOCK* scan;
+
+    scan = ui_new_scan();
+
+    while (1) {
+        printf("\r\nEnter the next value or");
+        printf("\r\n[i] increased");
+        printf("\r\n[d] decreased");
+        printf("\r\n[m] print matches");
+        printf("\r\n[p] poke address");
+        printf("\r\n[n] new scan");
+        printf("\r\n[q] quit");
+
+        fgets(s, sizeof(s), stdin);
+        printf("\r\n");
+
+        switch(s[0]) {
+            case 'i':
+                update_scan(scan, COND_INCREASED, 0);
+                printf("%d, mathes found\r\n", get_match_count(scan));
+                break;
+            case 'd':
+                update_scan(scan, COND_DECREASED, 0);
+                printf("%d, mathes found\r\n", get_match_count(scan));
+                break;
+            case 'm':
+                print_matches(scan);
+                break;
+            case 'p':
+                ui_poke(scan->hProc, scan->data_size);
+                break;
+            case 'n':
+                free_scan(scan);
+                scan = ui_new_scan();
+                break;
+            case 'q':
+                free_scan(scan);
+                return;
+            default:
+                val = str2int(s);
+                update_scan(scan, COND_EQUALS, val);
+                printf("%d matches found\r\n", get_match_count(scan));
+                break;
+        }
+    }
+}
+
+int main (int argc, char *argv[]) {
+    ui_run_scan();
     return 0;
 }
